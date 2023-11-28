9d2a335857546072222d00079e3168a4d563ede9
Taihenc <51960471+Taihenc@users.noreply.github.com>
2023-11-29 02:30:29 +0700

    re-write moe_detail and add basic text style

--- app/src/main/java/com/taihen/moekot/MainActivity.kt
+++ app/src/main/java/com/taihen/moekot/MainActivity.kt
@@ -10,24 +10,24 @@ import com.taihen.moekot.model.MoeItem
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.moe_detail)
+//        setContentView(R.layout.moe_detail)
 
-        val imageView = findViewById<ImageView>(R.id.moe_image)
-//        setContentView(R.layout.main_activity)
+//        val imageView = findViewById<ImageView>(R.id.moe_image)
+        setContentView(R.layout.main_activity)
 
-//        val recyclerView: RecyclerView = findViewById(R.id.main_recycler)
-//        val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(this, 3)
-//        recyclerView.layoutManager = layoutManager
-//
-//        val moeItems = listOf<MoeItem>(
-//            MoeItem(R.drawable.preview_chainsaw, "Chainsaw Man"),
-//            MoeItem(R.drawable.preview_jujutsu, "Jujutsu Kaisen"),
-//            MoeItem(R.drawable.preview_oshino, "Oshi no Ko"),
-//            MoeItem(R.drawable.preview_yofukashi, "Yofukashi no Uta"),
-//        )
-//
-//        println("moeItems: $moeItems")
-//        val adapter = MoeAdapter(moeItems)
-//        recyclerView.adapter = adapter
+        val recyclerView: RecyclerView = findViewById(R.id.main_recycler)
+        val layoutManager: RecyclerView.LayoutManager = GridLayoutManager(this, 3)
+        recyclerView.layoutManager = layoutManager
+
+        val moeItems = listOf<MoeItem>(
+            MoeItem(R.drawable.preview_chainsaw, "Chainsaw Man"),
+            MoeItem(R.drawable.preview_jujutsu, "Jujutsu Kaisen"),
+            MoeItem(R.drawable.preview_oshino, "Oshi no Ko"),
+            MoeItem(R.drawable.preview_yofukashi, "Yofukashi no Uta"),
+        )
+
+        println("moeItems: $moeItems")
+        val adapter = MoeAdapter(moeItems)
+        recyclerView.adapter = adapter
     }
 }
